35349615c3d2c14aefcd6fe5f0540f5c6a45b32e
glowredman <fredcraft00@gmail.com>
2022-07-05 20:00:02 +0200

    fix shading and update junixsocket

--- src/main/java/com/gitlab/cdagaming/craftpresence/utils/discord/rpc/entities/pipe/UnixPipe.java
+++ src/main/java/com/gitlab/cdagaming/craftpresence/utils/discord/rpc/entities/pipe/UnixPipe.java
@@ -48,7 +48,7 @@ public class UnixPipe extends Pipe {
         super(ipcClient, callbacks);
 
         socket = AFUNIXSocket.newInstance();
-        socket.connect(new AFUNIXSocketAddress(new File(location)));
+        socket.connect(AFUNIXSocketAddress.of(new File(location)));
     }
 
     @Override
